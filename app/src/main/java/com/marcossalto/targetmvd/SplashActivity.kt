2bfc09393012a85a00b743cac83fff3be2788ab9
Marcos Salto <marcosasalto@gmail.com>
2021-02-10 13:07:03 -0300

    Create Sign In Activity

--- app/src/main/java/com/marcossalto/targetmvd/SplashActivity.kt
+++ app/src/main/java/com/marcossalto/targetmvd/SplashActivity.kt
@@ -1,5 +1,6 @@
 package com.marcossalto.targetmvd
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 
@@ -8,6 +9,20 @@ class SplashActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
+        goToSignIn()
     }
 
+    private fun goToSignIn() {
+        val splash = object : Thread(){
+            override fun run(){
+                try{
+                    sleep(1500)
+                    val intent = Intent(this@SplashActivity, SignInActivity::class.java)
+                    finish()
+                    startActivity(intent)
+                }catch (e: Exception){}
+            }
+        }
+        splash.start()
+    }
 }
