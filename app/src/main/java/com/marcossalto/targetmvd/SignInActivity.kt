e579b9aa6635f205815d26f19941c616c17e63d1
Marcos Salto <marcosasalto@gmail.com>
2021-02-10 14:52:35 -0300

    Create Sign Up Activity

--- app/src/main/java/com/marcossalto/targetmvd/SignInActivity.kt
+++ app/src/main/java/com/marcossalto/targetmvd/SignInActivity.kt
@@ -1,5 +1,6 @@
 package com.marcossalto.targetmvd
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 
@@ -7,5 +8,20 @@ class SignInActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_sign_in)
+        goToSignUp()
+    }
+
+    private fun goToSignUp() {
+        val splash = object : Thread(){
+            override fun run(){
+                try{
+                    sleep(2500)
+                    val intent = Intent(this@SignInActivity, SignUpActivity::class.java)
+                    finish()
+                    startActivity(intent)
+                }catch (e: Exception){}
+            }
+        }
+        splash.start()
     }
 }
