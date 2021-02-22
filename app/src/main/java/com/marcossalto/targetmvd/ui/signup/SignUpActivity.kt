2e041dce1cdb9fae3cf2bd4010488aa4a05771bc
Marcos Salto <marcosasalto@gmail.com>
2021-02-22 11:17:50 -0300

    Add business logic to Sign Up Activity

--- app/src/main/java/com/marcossalto/targetmvd/ui/signup/SignUpActivity.kt
+++ app/src/main/java/com/marcossalto/targetmvd/ui/signup/SignUpActivity.kt
@@ -1,12 +1,181 @@
 package com.marcossalto.targetmvd.ui.signup
 
 import android.os.Bundle
-import androidx.appcompat.app.AppCompatActivity
+import android.view.View
+import android.widget.AdapterView
+import androidx.core.view.get
+import androidx.lifecycle.ViewModelProviders
 import com.marcossalto.targetmvd.R
+import com.marcossalto.targetmvd.databinding.ActivitySignUpBinding
+import com.marcossalto.targetmvd.metrics.Analytics
+import com.marcossalto.targetmvd.metrics.PageEvents
+import com.marcossalto.targetmvd.metrics.VISIT_SIGN_UP
+import com.marcossalto.targetmvd.network.models.UserSignUp
+import com.marcossalto.targetmvd.ui.base.BaseActivity
+import com.marcossalto.targetmvd.ui.profile.ProfileActivity
+import com.marcossalto.targetmvd.ui.signin.SignInActivity
+import com.marcossalto.targetmvd.ui.view.AuthView
+import com.marcossalto.targetmvd.util.NetworkState
+import com.marcossalto.targetmvd.util.ViewModelListener
+import com.marcossalto.targetmvd.util.extensions.*
 
-class SignUpActivity : AppCompatActivity() {
+class SignUpActivity : BaseActivity(), AuthView {
+
+    private lateinit var viewModel: SignUpActivityViewModel
+    private lateinit var binding: ActivitySignUpBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_sign_up)
+        binding = ActivitySignUpBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        Analytics.track(PageEvents.visit(VISIT_SIGN_UP))
+
+        val factory = SignUpActivityViewModelFactory(viewModelListener)
+        viewModel = ViewModelProviders.of(this, factory)
+            .get(SignUpActivityViewModel::class.java)
+
+        with(binding) {
+            nameEditText.setOnFocusChangeListener { _, b -> clearErrorState(b) }
+            emailEditText.setOnFocusChangeListener { _, b -> clearErrorState(b) }
+            passwordEditText.setOnFocusChangeListener { _, b -> clearErrorState(b) }
+            confirmPasswordEditText.setOnFocusChangeListener { _, b -> clearErrorState(b) }
+            genderSpinner.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
+                override fun onItemSelected(
+                    parent: AdapterView<*>,
+                    view: View,
+                    position: Int,
+                    id: Long
+                ) {
+                    clearErrorState(false)
+                }
+
+                override fun onNothingSelected(parent: AdapterView<*>?) {
+
+                }
+            }
+            signUpButtonSignUp.setOnClickListener { signUp() }
+            signInButtonSignUp.setOnClickListener { signIn() }
+        }
+
+        lifecycle.addObserver(viewModel)
+    }
+
+    override fun showProfile() {
+        startActivityClearTask(ProfileActivity())
+    }
+
+    private fun signUp() {
+        clearErrorState(false)
+        with(binding) {
+            when {
+                !nameEditText.isNotEmpty() -> {
+                    with(nameMessageTextView) {
+                        text = getString(R.string.you_forgot_to_put_your_name)
+                        visibility = android.view.View.VISIBLE
+                    }
+                    nameEditText.background = resources.getDrawable(R.drawable.edit_text_red)
+                }
+                !emailEditText.value().isEmail() -> {
+                    with(emailMessageTextView) {
+                        text = getString(R.string.oops_this_email_is_not_valid)
+                        visibility = android.view.View.VISIBLE
+                    }
+                    emailEditText.background = resources.getDrawable(R.drawable.edit_text_red)
+                }
+                !passwordEditText.validate("[a-zA-Z0-9]{6,}") -> {
+                    with(passwordMessageTextView) {
+                        text = getString(R.string.the_password_must_be_6_character_long)
+                        visibility = android.view.View.VISIBLE
+                    }
+                    passwordEditText.background = resources.getDrawable(R.drawable.edit_text_red)
+                }
+                confirmPasswordEditText.value() != passwordEditText.value() -> {
+                    with(confirmPasswordMessage) {
+                        text =
+                            getString(R.string.password_don_t_match)
+                        visibility = android.view.View.VISIBLE
+                    }
+                    confirmPasswordEditText.background =
+                        resources.getDrawable(R.drawable.edit_text_red)
+                }
+                genderSpinner.selectedItemId.toInt() == 0 -> {
+                    with(genderMessageTextView) {
+                        text =
+                            getString(R.string.you_forgot_to_select_your_gender)
+                        visibility = android.view.View.VISIBLE
+                    }
+                }
+                else -> {
+                    val userSignUp = UserSignUp(
+                        userName = nameEditText.value().replace(" ","").trim().toLowerCase(),
+                        email = emailEditText.value(),
+                        gender = genderSpinner.getItemAtPosition(genderSpinner.selectedItemPosition).toString().toLowerCase(),
+                        password = passwordEditText.value(),
+                        passwordConfirmation = confirmPasswordEditText.value()
+                    )
+                    viewModel.signUp(userSignUp)
+                }
+            }
+        }
+    }
+
+    private fun signIn() {
+        startActivityClearTask(SignInActivity())
+    }
+
+    // ViewModelListener
+    private val viewModelListener = object : ViewModelListener {
+        override fun updateState() {
+            when (viewModel.state) {
+                SignUpState.FAILURE -> showError(viewModel.error)
+                SignUpState.SUCCESS -> showProfile()
+                else -> {
+                }
+            }
+        }
+
+        override fun updateNetworkState() {
+            when (viewModel.networkState) {
+                NetworkState.LOADING -> showProgress()
+                NetworkState.IDLE -> hideProgress()
+                else -> showError(viewModel.error ?: getString(R.string.default_error))
+            }
+        }
+    }
+
+    override fun showError(message: String?) {
+        if (message != null) {
+            toast(message)
+        }
+    }
+
+    private fun clearErrorState(b: Boolean) {
+        with(binding) {
+            with(nameMessageTextView) {
+                text = ""
+                visibility = android.view.View.GONE
+            }
+            with(emailMessageTextView) {
+                text = ""
+                visibility = android.view.View.GONE
+            }
+            with(passwordMessageTextView) {
+                text = ""
+                visibility = android.view.View.GONE
+            }
+            with(confirmPasswordMessage) {
+                text = ""
+                visibility = android.view.View.GONE
+            }
+            with(genderMessageTextView) {
+                text = ""
+                visibility = android.view.View.GONE
+            }
+            nameEditText.background = resources.getDrawable(R.drawable.edit_text_normal)
+            emailEditText.background = resources.getDrawable(R.drawable.edit_text_normal)
+            passwordEditText.background = resources.getDrawable(R.drawable.edit_text_normal)
+            confirmPasswordEditText.background = resources.getDrawable(R.drawable.edit_text_normal)
+        }
     }
 }
---
+++ app/src/main/java/com/marcossalto/targetmvd/ui/signup/SignUpActivityViewModel.kt
@@ -0,0 +1,66 @@
+package com.marcossalto.targetmvd.ui.signup
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.viewModelScope
+import com.marcossalto.targetmvd.network.managers.IUserManager
+import com.marcossalto.targetmvd.network.managers.SessionManager
+import com.marcossalto.targetmvd.network.managers.UserManager
+import com.marcossalto.targetmvd.network.models.UserSignUp
+import com.marcossalto.targetmvd.ui.base.BaseViewModel
+import com.marcossalto.targetmvd.util.NetworkState
+import com.marcossalto.targetmvd.util.ViewModelListener
+import com.marcossalto.targetmvd.util.extensions.ApiErrorType
+import com.marcossalto.targetmvd.util.extensions.ApiException
+import kotlinx.coroutines.launch
+
+open class SignUpActivityViewModel(listener: ViewModelListener?) : BaseViewModel(listener) {
+
+    private val manager: IUserManager = UserManager
+
+    var state: SignUpState = SignUpState.NONE
+        set(value) {
+            field = value
+            listener?.updateState()
+        }
+
+    fun signUp(user: UserSignUp) {
+        networkState = NetworkState.LOADING
+        viewModelScope.launch {
+            val result = manager.signUp(user = user)
+
+            if (result.isSuccess) {
+                result.getOrNull()?.value?.user?.let { user ->
+                    SessionManager.signIn(user)
+                }
+
+                networkState = NetworkState.IDLE
+                state = SignUpState.SUCCESS
+            } else {
+                handleError(result.exceptionOrNull())
+            }
+        }
+    }
+
+    private fun handleError(exception: Throwable?) {
+        error = if (exception is ApiException && exception.errorType == ApiErrorType.API_ERROR) {
+            exception.message
+        } else null
+
+        networkState = NetworkState.IDLE
+        networkState = NetworkState.ERROR
+        state = SignUpState.FAILURE
+    }
+}
+
+enum class SignUpState {
+    FAILURE,
+    SUCCESS,
+    NONE,
+}
+
+class SignUpActivityViewModelFactory(var listener: ViewModelListener?) : ViewModelProvider.Factory {
+    override fun <T : ViewModel?> create(modelClass: Class<T>): T {
+        return SignUpActivityViewModel(listener) as T
+    }
+}
